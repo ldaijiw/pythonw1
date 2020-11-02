3d302fd3a24c0bc9b7af07140c290ed4e84f9660
ldaijiw <lwaltmann@spartaglobal>
2020-11-02 16:04:03 +0000

    datatypes and operators

---
+++ DataTypes_Operators.py
@@ -0,0 +1,20 @@
+# What are data types and operators
+
+# Booleans: True or False
+
+a = True
+b = False
+
+print(a==b)
+print(a!=b)
+
+
+greetings = "Hello World"
+
+# checking if alphabetical order
+print(greetings.isalpha())
+
+#checking if lower
+print(greetings.islower())
+print(greetings.startswith("H"))
+print(greetings.endswith("ld"))
