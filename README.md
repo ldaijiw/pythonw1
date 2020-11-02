75f0746eeed970ed971133f5416c5957fdc9e4b7
ldaijiw <lwaltmann@spartaglobal>
2020-11-02 15:05:17 +0000

    variables and input

---
+++ README.md
@@ -0,0 +1,8 @@
+# Python Week 1
+
+## Why Python
+
+**Variable**
+- is a placeholder to store the data
+
+**Pseudo Code**
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("hello world")
+
+# making a variable
+name = "leo"
+print(name)
+
+age = 21
+print(age)
+
+
+# How can we take user data => use input()
+
+lastname = input("please enter your last name: ")
+print(lastname)
+
