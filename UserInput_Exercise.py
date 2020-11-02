981eec96d59e3c29721956d00be0f932394d7a12
ldaijiw <lwaltmann@spartaglobal>
2020-11-02 15:39:11 +0000

    added print for var types

--- UserInput_Exercise.py
+++ UserInput_Exercise.py
@@ -2,7 +2,11 @@
 
 name = input("What is your name: ")
 dob = input("What is your date of birth: ")
-age = input("How old are you: ")
+age = int(input("How old are you: "))
 
 
 print(f'Hi {name}, you were born in {dob}, you are {age} years old.')
+print('Types of variable:')
+print('name: ', type(name))
+print('dob: ', type(dob))
+print('age: ', type(age))
